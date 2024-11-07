a8cfc27aa1159f272dce78ea5dcfb9f675b0ced0
Yuval-Waisemberger <yuval.waisemberger@runi.post.ac.il>
2024-11-07 18:40:57 +0200

    Added Numords file

--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,13 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    
+		int numberGiven = Integer.parseInt(args[0]);
+		int hundreds = (numberGiven / 100);
+		int tens = ((numberGiven / 10) % 10);
+		int ones = numberGiven % 10;
+
+		System.out.println(hundreds + " hundreds, " + tens + " tens, " +
+							"and " + ones + " ones.");
 	}
 }
